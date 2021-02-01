ed310d2b49a5e0f6f342c7f76d37c2a722796485
ManceloAngela <marcelo.andrade.triade@gmail.com>
2021-01-31 22:56:53 -0300

    first

---
+++ src/test/java/com/treinamento/springudemy/SpringudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.springudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
