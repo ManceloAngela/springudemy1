314a36eecdcda5dcafb5d429c2d3005f9a75858e
ManceloAngela <marcelo.andrade.triade@gmail.com>
2021-02-06 15:21:55 -0300

    Add env

---
+++ src/main/java/com/treinamento/springudemy/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.treinamento.springudemy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig{
+  
+
+  private static final String CONTACT_EMAIL = "marcelo_eng@terra.com.br";
+  private static final String CONTACT_GITHUB = "";
+  private static final String CONTACT_NAME = "Marcelo Andrade";
+  private static final String API_VERSION = "1.0.0";
+  private static final String API_DESCRIPTION = "Itegrator Kenoby API Professinal";
+  private static final String API_TITLE = "Integrator Kenoby/Senior - Recruitment MDS Insure";
+  private static final String BASE_PAKAGE = "com.treinamento.springudemy";
+
+  @Bean
+  public Docket api(){
+    return new Docket(DocumentationType.SWAGGER_2)
+    .select()
+    .apis(RequestHandlerSelectors.basePackage(BASE_PAKAGE))
+    .paths(PathSelectors.any())
+    .build()
+    .apiInfo(buildApiInfo());
+  }
+
+  private ApiInfo buildApiInfo() {
+    return new ApiInfoBuilder()
+    .title(API_TITLE)
+    .description(API_DESCRIPTION)
+    .version(API_VERSION)
+    .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB,CONTACT_EMAIL))
+    .build();
+  }
+
+}
---
+++ src/main/java/com/treinamento/springudemy/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.treinamento.springudemy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+  @ApiOperation(value = "Return exemple hello world")
+  @ApiResponses(value = {
+    @ApiResponse(code = 200,message = "Success method return")
+  })
+  @GetMapping
+  public String hello(){
+    return "Hello Bookstyore Manager.";
+  }
+  
+}
